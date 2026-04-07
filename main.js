249de9610678373282b787b6caf8d495ac58238e
danielc3165-sketch <daniel.c3165@gmail.com>
2026-04-07 21:49:28 +0300

    only j.s

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 'use strict';
 
-function showMessage() {
-   console.log('Hello, World!');
+var height=100;
+
+function showMessage(el) {
+    height+=50;
+    if(height>400) height=100
+   
+el.style.height=height+'px';
+    el.innerText=height+'px';
+    
+    //console.log(el.style.height);
 }
